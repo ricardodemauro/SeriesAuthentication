ed2629b3783e59895940aac0006195758a61d3bd
Ricardo Mauro <ricardo.demauro@outlook.com>
2020-05-28 18:47:31 -0300

    adding views for email confirmation

---
+++ src/WebAppIdentityMvc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppIdentityMvc;
+using WebAppIdentityMvc.Models;
+
+[assembly: HostingStartup(typeof(WebAppIdentityMvc.Areas.Identity.IdentityHostingStartup))]
+namespace WebAppIdentityMvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
